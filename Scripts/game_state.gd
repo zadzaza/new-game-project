d2d50c4f8024a1d096a3d642a4e10762a557be13
zadzaza <85344142+zadzaza@users.noreply.github.com>
2023-03-05 02:08:21 +0300

    git add

--- Scripts/game_state.gd
+++ Scripts/game_state.gd
@@ -5,10 +5,10 @@ var IronKey = "Iron Key"
 var meet = false
 
 func _process(delta):
-	has_key()
+	check_key()
 	print(has_key)
 
-func has_key():
-	for IronKey in range(10):
+func check_key():
+	for IronKey in PlayerInventory.inventory:
 		if PlayerInventory.inventory.has(IronKey) == true:
 			has_key = true
