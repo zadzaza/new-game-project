bf223842bf2c3334bd0030652c46101cae0d0a22
zadzaza <85344142+zadzaza@users.noreply.github.com>
2023-03-08 19:08:03 +0300

    git add

--- Scripts/RoomWithCorpse.gd
+++ Scripts/RoomWithCorpse.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 func _ready():
-	$Player.flip_h = false
-	var start = preload("res://Dialogues/start.tres")
-	DialogueManager.show_example_dialogue_balloon("start", start)
+	#$Player.flip_h = false
+	#var start = preload("res://Dialogues/start.tres")
+	#DialogueManager.show_example_dialogue_balloon("start", start)
+	pass
