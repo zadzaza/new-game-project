35333a96c0f5b257ecaf4c4f3a92fb03a2c6ec6f
zadzaza <85344142+zadzaza@users.noreply.github.com>
2023-03-10 14:28:34 +0300

    git add

--- Scripts/Library.gd
+++ Scripts/Library.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 func _on_Door_entered(body):
 	get_tree().change_scene("res://Scenes/PortraitRoom.tscn")
+	GlobalScript.last_location = "Library"
 
 
 func _on_ToStreet_body_entered(body):
--- Scripts/MainRoom.gd
+++ Scripts/MainRoom.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+var start = preload("res://Dialogues/start.tres")
+
 func _ready():
 	if GlobalScript.last_location == "":
 		$YSort/Player.position = Vector2(958, 76)
 		$YSort/Player.animation = "idle_down"
+		DialogueManager.show_example_dialogue_balloon("start", start)
+	if GlobalScript.last_location == "Library":
+		$YSort/Player.position = Vector2(1126, 349)
+		$YSort/Player.animation = "idle_side"
+		$YSort/Player.flip_h = true
 
 func _on_Door_entered(body):
 	get_tree().change_scene("res://Scenes/Library.tscn")
