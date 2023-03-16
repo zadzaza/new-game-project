d476bf1fb7602de6a04ff4ba4c02b3c517fa555a
zadzaza <85344142+zadzaza@users.noreply.github.com>
2023-03-17 01:09:57 +0300

    git add

--- Scripts/Lamp.gd
+++ Scripts/Lamp.gd
@@ -6,4 +6,11 @@ func _on_Area2D_body_entered(body):
 	pass
 
 func _on_Area2D_body_exited(body):
-	
+	button.show_but_e = false
+
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("e"):
+		
