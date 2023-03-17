b2506ff055521e2a01bd87b4fc61790ff3c63c03
zadzaza <85344142+zadzaza@users.noreply.github.com>
2023-03-17 05:28:14 +0300

    git add

--- Scripts/InventoryNew.gd
+++ Scripts/InventoryNew.gd
@@ -29,9 +29,64 @@ func _input(event):
 	if event.is_action_pressed("f") and GlobalScript.is_pause == false:
 		GlobalScript.is_pause = true
 		if GlobalScript.pause() == 1:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			show()
 	elif event.is_action_pressed("f") and GlobalScript.is_pause == true:
 		GlobalScript.is_pause = false
 		GlobalScript.pause(0)
 		hide()
+
+
+func _on_Coat_mouse_entered():
+	if $Balloon/Margin2/Realization/Coat.bbcode_text != done:
+		$Balloon/Margin2/Realization/Coat.bbcode_text = "Спрятать пальто"
+func _on_Coat_mouse_exited():
+	if $Balloon/Margin2/Realization/Coat.bbcode_text != done:
+		$Balloon/Margin2/Realization/Coat.bbcode_text = not_done
+
+
+func _on_Valise_mouse_entered():
+	if $Balloon/Margin2/Realization/Valise.bbcode_text != done:
+		$Balloon/Margin2/Realization/Valise.bbcode_text = "Спрятать саквояж"
+func _on_Valise_mouse_exited():
+	if $Balloon/Margin2/Realization/Valise.bbcode_text != done:
+		$Balloon/Margin2/Realization/Valise.bbcode_text = not_done
+
+
+func _on_Corpse_mouse_entered():
+	if $Balloon/Margin2/Realization/Corpse.bbcode_text != done:
+		$Balloon/Margin2/Realization/Corpse.bbcode_text = "Сжечь труп"
+func _on_Corpse_mouse_exited():
+	if $Balloon/Margin2/Realization/Corpse.bbcode_text != done:
+		$Balloon/Margin2/Realization/Corpse.bbcode_text = not_done
+
+
+func _on_Table_mouse_entered():
+	if $Balloon/Margin2/Realization/Table.bbcode_text != done:
+		$Balloon/Margin2/Realization/Table.bbcode_text = "Протереть тряпкой"
+func _on_Table_mouse_exited():
+	if $Balloon/Margin2/Realization/Table.bbcode_text != done:
+		$Balloon/Margin2/Realization/Table.bbcode_text = not_done
+
+
+func _on_Lamp_mouse_entered():
+	if $Balloon/Margin2/Realization/Lamp.bbcode_text != done:
+		$Balloon/Margin2/Realization/Lamp.bbcode_text = "Вернуть на место"
+func _on_Lamp_mouse_exited():
+	if $Balloon/Margin2/Realization/Lamp.bbcode_text != done:
+		$Balloon/Margin2/Realization/Lamp.bbcode_text = not_done
+
+
+func _on_Portrait_mouse_entered():
+	if $Balloon/Margin2/Realization/Portrait.bbcode_text != done:
+		$Balloon/Margin2/Realization/Portrait.bbcode_text = "Завесить портрет"
+func _on_Portrait_mouse_exited():
+	if $Balloon/Margin2/Realization/Portrait.bbcode_text != done:
+		$Balloon/Margin2/Realization/Portrait.bbcode_text = not_done
+
+
+func _on_Knife_mouse_entered():
+	if $Balloon/Margin2/Realization/Knife.bbcode_text != done:
+		$Balloon/Margin2/Realization/Knife.bbcode_text = "Спрятать нож"
+func _on_Knife_mouse_exited():
+	if $Balloon/Margin2/Realization/Knife.bbcode_text != done:
+		$Balloon/Margin2/Realization/Knife.bbcode_text = not_done
