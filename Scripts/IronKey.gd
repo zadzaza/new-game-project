d4ecc83883994f1fe747cb9a5ae3c467b62fd0e9
vladsvarchik <106384661+vladsvarchik@users.noreply.github.com>
2023-03-17 23:06:34 +0300

    git add

---
+++ Scripts/IronKey.gd
@@ -0,0 +1,32 @@
+		extends KinematicBody2D
+
+const max_speed = 1000
+const acceleration = 500
+var item_name
+var item_quantity
+var velocity = Vector2()
+
+var player = null
+var being_picked_up = false
+
+func _ready():
+	item_name = "Iron Key"
+
+func _physics_process(delta):
+	if being_picked_up == false:
+		#velocity = velocity.move_toward(Vector2(0, max_speed), acceleration * delta)
+		pass
+	else:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+		
+		var distance = global_position.distance_to(player.global_position)
+		if distance < 10:
+			PlayerInventory.add_item(item_name, 1)
+			queue_free()
+		
+	velocity = move_and_slide(velocity, Vector2())
+
+func pick_up_item(body):
+	player = body
+	being_picked_up = true
--- Scripts/Rag.gd
+++ Scripts/Rag.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+onready var button = get_node("Button")
+var overlapping_bodies
 
+func _process(delta):
+	overlapping_bodies = $Area2D.get_overlapping_bodies()
+	
+	if overlapping_bodies.size() > 0:
+		if GameState.has_rag == false:
+			button.show_but_e = true
+		if GameState.has_rag == true:
+			button.show_but_e = false
+
+func _ready():
+	if GameState.has_rag == true:
+		hide()
 
 func _on_Area2D_body_entered(body):
 	pass # Replace with function body.
+
+func _on_Area2D_body_exited(body):
+	button.show_but_e = false
+
 
 func _input(event):
 	if event.is_action_pressed("e"):
